d6514c04fd677b8671b12d9f9b1b3d0b9a173fca
Vityan70 <viktor19962010@gmail.com>
2021-12-09 23:20:26 +0700

    4

--- src/hoc/layout/layout.js
+++ src/hoc/layout/layout.js
@@ -1,12 +1,41 @@
 import React, {Component} from 'react'
 // eslint-disable-next-line
 import classes from './Layout.css'
+import MenuToggle from '../../components/Navigation/MenuToggle/MenuToggle'
+import Drawer from '../../components/Navigation/Drawer/Drawer'
 
 
 class Layout extends Component {
+
+    state = {
+        menu: false
+    }
+
+    toggleMenuHandler = () => {
+        this.setState({
+            menu: !this.state.menu
+        })
+    }
+
+    menuCloseHandler = () => {
+        this.setState({
+            menu: false
+        })
+    }
+
     render () {
         return (
             <div className='Layout'>
+
+                <Drawer
+                    isOpen={this.state.menu}
+                    onClose={this.menuCloseHandler}
+                />
+
+                <MenuToggle
+                    onToggle={this.toggleMenuHandler}
+                    isOpen={this.state.menu}
+                />
                 <main className='main'>
                     { this.props.children }
                 </main>
